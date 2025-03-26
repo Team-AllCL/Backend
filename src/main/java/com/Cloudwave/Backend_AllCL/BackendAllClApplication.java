db64cc898843f8744391024e7c63ed523166bcda
chaeeun1103 <chaeeun1103@inu.ac.kr>
2025-03-26 23:19:48 +0900

    spring boot 개발환경 설정 및 mysql 연동

---
+++ src/main/java/com/Cloudwave/Backend_AllCL/BackendAllClApplication.java
@@ -0,0 +1,13 @@
+package com.Cloudwave.Backend_AllCL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAllClApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAllClApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Cloudwave/Backend_AllCL/BackendAllClApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Cloudwave.Backend_AllCL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAllClApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
