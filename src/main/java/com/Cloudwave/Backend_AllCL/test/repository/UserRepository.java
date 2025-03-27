3c6b34947d0b1d9446448e8646e15bad4cc6f5ab
chaeeun1103 <chaeeun1103@inu.ac.kr>
2025-03-27 18:45:53 +0900

    test 코드 삭제

--- src/main/java/com/Cloudwave/Backend_AllCL/test/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Cloudwave.Backend_AllCL.test.repository;
-
-import com.Cloudwave.Backend_AllCL.test.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}
