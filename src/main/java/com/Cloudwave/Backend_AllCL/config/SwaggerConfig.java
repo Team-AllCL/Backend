5e92160509e588e3ec6b6ea8967e351d7c214b59
chaeeun1103 <chaeeun1103@inu.ac.kr>
2025-03-28 11:46:09 +0900

    swagger 설정

---
+++ src/main/java/com/Cloudwave/Backend_AllCL/config/SwaggerConfig.java
@@ -0,0 +1,15 @@
+package com.Cloudwave.Backend_AllCL.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Cloudwave 5기 AllCL",
+                version = "v1.0",
+                description = "Team AllCL Backend Swagger 문서입니다."
+        )
+)
+
+public class SwaggerConfig {
+}
