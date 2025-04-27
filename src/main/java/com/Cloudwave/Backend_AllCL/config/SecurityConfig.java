a18ecb30c209dfa2ddc1d4b848ea18f22d887d7b
chaeeun1103 <chaeeun1103@inu.ac.kr>
2025-04-28 01:03:11 +0900

    SecurityConfig 추가

---
+++ src/main/java/com/Cloudwave/Backend_AllCL/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.Cloudwave.Backend_AllCL.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors -> cors.disable())     // CORS 비활성화
+                .csrf(csrf -> csrf.disable())     // CSRF 비활성화
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()     // 모든 요청 허용
+                );
+        return http.build();
+    }
+}
