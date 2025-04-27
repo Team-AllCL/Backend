14630fa907a748c15bcc5f76a8de5b818717cdc1
yunss719 <ys26@inu.ac.kr>
2025-04-27 21:52:51 +0900

    t10 테스트

--- src/main/java/com/Cloudwave/Backend_AllCL/config/WebConfig.java
+++ src/main/java/com/Cloudwave/Backend_AllCL/config/WebConfig.java
@@ -10,6 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
+         "http://dkgmplk5bgam1.cloudfront.net",
                     "http://localhost:3000",
                     "http://localhost:3001",
                     "http://localhost:3002"
