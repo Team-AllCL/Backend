30593692113810df359714ce4cd6c4ff134a2d8c
Chaeeun <164310681+chaeeun1103@users.noreply.github.com>
2025-04-28 01:32:31 +0900

    Update SecurityConfig.java

--- src/main/java/com/Cloudwave/Backend_AllCL/config/SecurityConfig.java
+++ src/main/java/com/Cloudwave/Backend_AllCL/config/SecurityConfig.java
@@ -9,13 +9,26 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .cors(cors -> cors.disable()) // WebConfig에서 CORS 설정 가져오기
-                .csrf(csrf -> csrf.disable()) // CSRF 비활성화
-                .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().permitAll() // 모든 요청 허용
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors -> cors
+                        .configurationSource(request -> {
+                            var corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
+                            corsConfiguration.addAllowedOrigin("http://localhost:3000");
+                            corsConfiguration.addAllowedOrigin("http://localhost:3001");
+                            corsConfiguration.addAllowedOrigin("http://localhost:3002");
+                            corsConfiguration.addAllowedOrigin("http://dkgmp1k5bgam1.cloudfront.net");
+                            corsConfiguration.addAllowedMethod("*");
+                            corsConfiguration.addAllowedHeader("*");
+                            corsConfiguration.setAllowCredentials(true);
+                            return corsConfiguration;
+                        })
                 )
-                .build();
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
     }
 }
