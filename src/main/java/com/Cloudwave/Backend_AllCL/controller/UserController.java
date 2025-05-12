ae6b8fc923b697e39e44ce8b2f704785451783e7
jiyoonkim <rlawldbs1007@gamil.com>
2025-05-12 16:54:39 +0900

    로그인 부분 수정

--- src/main/java/com/Cloudwave/Backend_AllCL/controller/UserController.java
+++ src/main/java/com/Cloudwave/Backend_AllCL/controller/UserController.java
@@ -5,6 +5,8 @@ import com.Cloudwave.Backend_AllCL.entity.User;
 import com.Cloudwave.Backend_AllCL.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 @CrossOrigin(origins = "http://dkgmp1k5bgam1.cloudfront.net")
 @RestController
@@ -19,7 +21,19 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public User registerUser(@RequestBody UserDto userDto) {
+    public User registerOrLogin(@RequestBody UserDto userDto) {
+        User existingUser = userService.findByEmail(userDto.getEmail());
+
+        if (existingUser != null) {
+            // 이미 존재하는 유저일 경우 → 비밀번호 검증
+            if (existingUser.getPassword().equals(userDto.getPassword())) {
+                return existingUser; // 로그인 성공
+            } else {
+                throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "비밀번호가 틀렸습니다.");
+            }
+        }
+
+        // 존재하지 않으면 새로 가입
         return userService.createUser(userDto);
     }
 }
