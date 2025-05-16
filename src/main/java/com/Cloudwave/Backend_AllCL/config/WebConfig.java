03e71e5cc955dc61e0a2baff38321c995cb3ec0a
Chaeeun <164310681+chaeeun1103@users.noreply.github.com>
2025-05-16 16:42:56 +0900

    Update WebConfig.java

--- src/main/java/com/Cloudwave/Backend_AllCL/config/WebConfig.java
+++ src/main/java/com/Cloudwave/Backend_AllCL/config/WebConfig.java
@@ -14,8 +14,8 @@ public class WebConfig implements WebMvcConfigurer {
                         "http://localhost:3001",
                         "http://localhost:3002",
                         "http://dkgmp1k5bgam1.cloudfront.net",
-                        "https://api.allcle-order.click",
-                        "https://api.allcle-ticketing.click"
+                        "https://allcle-order.click",
+                        "https://allcle-ticketing.click"
 
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
